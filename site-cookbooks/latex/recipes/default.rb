87c65168c9b78c5b67a5c7cf8fc054b68c7700ea
TakuyaHitomi <takuya.0101@gmail.com>
2014-01-31 21:26:19 +0900

    added some packages

--- site-cookbooks/latex/recipes/default.rb
+++ site-cookbooks/latex/recipes/default.rb
@@ -7,6 +7,22 @@
 # All rights reserved - Do Not Redistribute
 #
 
+
+package "software-properties-common" do
+  action :install
+end
+
+package "python-software-properties" do
+  action :install
+end
+
+bash "add ppa" do
+  code <<-EOC
+    sudo add-apt-repository ppa:texlive-backports/ppa
+    sudo apt-get update
+  EOC
+end
+
 package "texlive-lang-cjk" do
   action :install
 end
