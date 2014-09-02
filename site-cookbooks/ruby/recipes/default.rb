dbbf88a9e303f759d8358fa0b1f108c702c67f1f
TakuyaHitomi <takuya.0101@gmail.com>
2014-09-02 16:35:40 +0900

    deleted ruby install recipe

--- site-cookbooks/ruby/recipes/default.rb
+++ site-cookbooks/ruby/recipes/default.rb
@@ -45,15 +45,3 @@ git "/usr/local/rbenv/plugins/ruby-build" do
   group "rbenv"
 end
 
-# rubyをインストール
-execute "ruby install" do
-  not_if "source /etc/profile.d/rbenv.sh; rbenv versions | grep #{node.build}"
-  command "source /etc/profile.d/rbenv.sh; rbenv install #{node.build}"
-  action :run
-end
-
-#globalの切り替え
-execute "ruby change" do
-  command "source /etc/profile.d/rbenv.sh; rbenv global #{node.build};rbenv rehash"
-  action :run
-end
