2d679261f4819aa29fbac10d6c0b3b0fec8ce063
TakuyaHitomi <takuya.0101@gmail.com>
2014-09-02 17:41:34 +0900

    trivial modifications

--- site-cookbooks/ruby/recipes/default.rb
+++ site-cookbooks/ruby/recipes/default.rb
@@ -42,18 +42,6 @@ template "/etc/profile.d/rbenv.sh" do
   mode 0644
 end
 
-execute "ruby install" do
-  not_if "source /etc/profile.d/rbenv.sh; rbenv versions | grep #{node.build}"
-  command "source /etc/profile.d/rbenv.sh; rbenv install #{node.build}"
-  action :run
-end
-
-#globalの切り替え
-execute "ruby change" do
-  command "source /etc/profile.d/rbenv.sh; rbenv global #{node.build};rbenv rehash"
-  action :run
-end
-
 package "sqlite-devel" do
   action :install
 end
